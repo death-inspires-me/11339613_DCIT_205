b8282f3ff6012e05a13dc0fe7273af4d6fe58dc1
death-inspires-me <nana.esseney@gmail.com>
2023-11-17 12:57:14 +0000

    Remove slideshow, added normal header. Simpler is better in some cases.

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-let slideIndex = 0;
-showSlides();
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
